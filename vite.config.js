0c14a8ad9f243983d510c5a00d02813fe57022b4
Mrcavas <sidorov.mrcavas@mail.ru>
2024-01-29 19:44:30 +0700

    SharedArrayBuffer test 2

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,24 @@ import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    headers: {
-      "Cross-Origin-Opener-Policy": "same-origin",
-      "Cross-Origin-Embedder-Policy": "require-corp",
-      "Cross-Origin-Resource-Policy": "same-site",
+  plugins: [
+    react(),
+    {
+      name: "configure-response-headers",
+      configureServer: server => {
+        server.middlewares.use((_req, res, next) => {
+          res.setHeader("Cross-Origin-Embedder-Policy", "require-corp")
+          res.setHeader("Cross-Origin-Opener-Policy", "same-origin")
+          next()
+        })
+      },
     },
-  },
+  ],
+  // server: {
+  //   headers: {
+  //     "Cross-Origin-Opener-Policy": "same-origin",
+  //     // "Cross-Origin-Embedder-Policy": "require-corp",
+  //     "Cross-Origin-Resource-Policy": "same-site",
+  //   },
+  // },
 })
