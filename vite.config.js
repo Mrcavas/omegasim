9149c12786879ad2c249eca6700dc696d50375ed
Mrcavas <sidorov.mrcavas@mail.ru>
2024-01-29 19:51:24 +0700

    vercel header config 2

--- vite.config.js
+++ vite.config.js
@@ -16,4 +16,10 @@ export default defineConfig({
       },
     },
   ],
+  server: {
+    headers: {
+      "Cross-Origin-Embedder-Policy": "require-corp",
+      "Cross-Origin-Opener-Policy": "same-origin",
+    },
+  },
 })
